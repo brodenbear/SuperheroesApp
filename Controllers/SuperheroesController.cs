a7889c72733b60f3d39b5e0f88fd95bbf9926459
Broden Berrett <brodenb@gmail.com>
2023-07-04 15:15:18 -0600

    created controllers and views

---
+++ Controllers/SuperheroesController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperheroesApp.Models;
+
+namespace SuperheroesApp.Controllers
+{
+    public class SuperheroesController : Controller
+    {
+        // GET: SuperheroesController
+        public ActionResult Index()
+        {
+
+            //LINQ query to retrieve all rows from table
+            var superheroes = ""; 
+            return View();
+        }
+
+        // GET: SuperheroesController/Details/5
+        public ActionResult Details(int id)
+        {
+
+            //LINQ Query to find specific row from table
+            return View();
+        }
+
+        // GET: SuperheroesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Superhero superhero)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
