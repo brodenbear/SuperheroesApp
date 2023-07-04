ca52a913708b5b0298e1f0e3f77dd419905386ca
Broden Berrett <brodenb@gmail.com>
2023-07-04 14:51:31 -0600

    AddedMySQL code to add database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperheroesApp.Models;
 
 namespace SuperheroesApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Superhero> Superheroes  { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
