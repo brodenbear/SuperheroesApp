f71293b269367838ea718daa77195c07815f47f9
Broden Berrett <brodenb@gmail.com>
2023-07-04 20:04:03 -0600

    updated the delete, edit and create methods on SuperheroesController

--- Controllers/SuperheroesController.cs
+++ Controllers/SuperheroesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using SuperheroesApp.Data;
 using SuperheroesApp.Models;
 
@@ -48,20 +50,36 @@ namespace SuperheroesApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Superhero superhero)
         {
-            try
+            if (ModelState.IsValid)
             {
+                Superhero newSuperhero = new Superhero
+                {
+                    Name = superhero.Name,
+                    PrimaryAbility = superhero.PrimaryAbility,
+                    SecondaryAbility = superhero.SecondaryAbility,
+                    AlterEgo = superhero.AlterEgo,
+                    Catchphrase = superhero.Catchphrase,
+                };
+
+                _context.Superheroes.Add(newSuperhero);
+                _context.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+
+            return View(superhero);
         }
+    
 
         // GET: SuperheroesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var superhero = _context.Superheroes.Find(id);
+            if (superhero == null)
+            {
+                return NotFound();
+            }
+            return View(superhero);
         }
 
         // POST: SuperheroesController/Edit/5
@@ -71,7 +89,27 @@ namespace SuperheroesApp.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    var existingSuperhero = _context.Superheroes.Find(id);
+                    if (existingSuperhero == null)
+                    {
+                        return NotFound();
+                    }
+
+                    // Update the properties of the existingSuperhero entity based on the submitted form data
+                    existingSuperhero.Name = superhero.Name;
+                    existingSuperhero.PrimaryAbility = superhero.PrimaryAbility;
+                    existingSuperhero.SecondaryAbility = superhero.SecondaryAbility;
+                    existingSuperhero.AlterEgo = superhero.AlterEgo;
+                    existingSuperhero.Catchphrase = superhero.Catchphrase;
+
+                    _context.SaveChanges();
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(superhero);
             }
             catch
             {
@@ -82,7 +120,13 @@ namespace SuperheroesApp.Controllers
         // GET: SuperheroesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var superhero = _context.Superheroes.Find(id);
+            if (superhero == null)
+            {
+                return NotFound();
+            }
+
+            return View(superhero);
         }
 
         // POST: SuperheroesController/Delete/5
@@ -92,6 +136,9 @@ namespace SuperheroesApp.Controllers
         {
             try
             {
+                _context.Superheroes.Remove(superhero);
+                _context.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
